848b081b0c83c46e541e3af1c17dd49e4fcea041
Johnny Gregório <johkker@gmail.com>
2021-08-12 13:26:40 -0300

    Identaçao corrigida

--- scripts.js
+++ scripts.js
@@ -1,31 +1,31 @@
 function tabuadaDeDez() {
     let tabuada = []
     const n = 10
-        for(i = 0; i <= n; i++) {
-            tabuada.push([i])
-            for(j = 0; j <=n; j++) {
-                
-                tabuada[i][j] = j*i
-            
-            }
+    for (i = 0; i <= n; i++) {
+        tabuada.push([i])
+        for (j = 0; j <= n; j++) {
+
+            tabuada[i][j] = j * i
+
         }
-        
-        console.table(tabuada)
+    }
+
+    console.table(tabuada)
 }
 
 // Função bônus
 
 function tabuadaDinamica(n) {
     let tabuada = []
-    
-        for(i = 0; i <= n; i++) {
-            tabuada.push([i])
-            for(j = 0; j <=n; j++) {
-                
-                tabuada[i][j] = j*i
-            
-            }
+
+    for (i = 0; i <= n; i++) {
+        tabuada.push([i])
+        for (j = 0; j <= n; j++) {
+
+            tabuada[i][j] = j * i
+
         }
-        
-        console.table(tabuada)
+    }
+
+    console.table(tabuada)
 }
